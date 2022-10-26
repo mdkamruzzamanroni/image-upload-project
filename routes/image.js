b8a7025cb2302fd45845aa01e340efd8e29228b2
Md Kamruzzaman <kamruzzaman.dte@gmail.com>
2022-10-26 11:53:01 +0600

    image resize

--- routes/image.js
+++ routes/image.js
@@ -2,6 +2,14 @@ const express=require('express');
 const router=express.Router();
 const multer= require("multer");
 
+// for image resizing require below 3 packages
+
+const imagemin = require('imagemin');
+const imageminJpegtran = require('imagemin-jpegtran');
+const imageminPngquant = require('imagemin-pngquant');
+ 
+
+
 router.post("/image-upload",async(req,res)=>{
     try{
         const storage= multer.diskStorage({
@@ -12,7 +20,7 @@ router.post("/image-upload",async(req,res)=>{
             callback(null,file.originalname)
         },
         });
-        const maxSize=6*1024*1024;//for 6MB
+        const maxSize=1*1024*1024;//for 6MB
         const upload=multer({
             storage:storage,
             fileFilter: (req, file, cb)=> {
@@ -31,10 +39,26 @@ router.post("/image-upload",async(req,res)=>{
           }).array('photos',12)
 
 //req.body= name, father name, age  req.file=image req.files= images
-          upload(req,res, (error)=> {  
+//SyntaxError: await is only valid in async function
+          upload(req,res, async(error)=> {  
             console.log("body test", req.body);
             console.log("files test", req.files);
 
+// for image resizing
+const files = await imagemin(['public/media/*.{jpg,png,jpeg,webp}'], {
+	destination: 'public/media',
+	plugins: [
+		imageminJpegtran(),
+		imageminPngquant({
+			quality: [0.6, 0.8]
+		})
+	]
+});
+   console.log("===>",files);
+
+   res.download(files[0].destinationPath);
+
+   //error handling of Multer 
             if (error instanceof multer.MulterError) {        
                 res.status(400).json({
                   status:"Fail",
